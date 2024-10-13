bf6031ca62260fb33609a4a46db6ece2adf42632
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-10-13 11:11:01 +0300

    Add a boolean and an int

--- src/Headphones.java
+++ src/Headphones.java
@@ -1,2 +1,11 @@
 public class Headphones {
+
+    String charge = "Micro usb";
+    String [] controls = {"power", "volume", "skip", "play/pause"};
+    String color = "Red/black";
+
+    static  boolean power = false;
+    static  int volume = 0;
+
+
 }
